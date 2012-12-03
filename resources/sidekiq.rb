49f72aa24925d5c49c339948d44a3e47d62f176d
Aaron D. Valade <adv@alum.mit.edu>
2012-12-03 13:46:01 +0800

    added rabbitmq and mailer_default environment variables

--- resources/sidekiq.rb
+++ resources/sidekiq.rb
@@ -4,7 +4,9 @@ attribute :role, :kind_of => [String, NilClass], :default => nil
 attribute :namespace, :kind_of => String, :default => "sidekiq"
 attribute :concurrency, :kind_of => Integer, :default => 25
 attribute :redis_url, :kind_of => String, :default => "redis://localhost:6379/1"
+attribute :rabbitmq_url, :kind_of => String, :default => "amqp://guest:guest@localhost/"
 attribute :namespace, :kind_of => String, :default => "sidekiq"
+attribute :mailer_default_url, :kind_of => String, :default => "medsensehq.com"
 attribute :bundler, :kind_of => [TrueClass, FalseClass, NilClass], :default => nil
 attribute :bundle_command, :kind_of => [String, NilClass], :default => "bundle"
 
