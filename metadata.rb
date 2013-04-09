0a6cb38dbac5dbac30f6a1fecd9b1a5bce287f4f
Aaron Valade <adv@dax.local>
2013-04-09 17:21:38 +0800

    updated to work with chef-solo

--- metadata.rb
+++ metadata.rb
@@ -4,10 +4,8 @@ maintainer_email "cookbooks@opscode.com"
 license          "Apache 2.0"
 description      "Deploys and configures Ruby-based applications"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.1.0"
+version          "1.1.1"
 
-%w{ application unicorn apache2 passenger_apache2 }.each do |cb|
+%w{ application unicorn apache2 passenger_apache2 runit }.each do |cb|
   depends cb
 end
-
-depends "runit", "~> 1.0"
